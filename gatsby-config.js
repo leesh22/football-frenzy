ab733e93adf63c3f965b4114248083d49e9e9a98
Alicia Collymore <alicihcoll@gmail.com>
2019-06-18 11:20:29 +0700

    basic configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: "/football-frenzy",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Football Frenzy`,
+    description: `Your one stop shop for all you football production needs`,
+    author: `@leesh22`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,6 +28,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
